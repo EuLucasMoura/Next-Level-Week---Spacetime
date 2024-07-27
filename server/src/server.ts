fc8c066a0e58418de25c6beade2e16a082ae5e8d
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2024-07-27 19:54:46 -0300

    adding prisma to project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,18 @@
-import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import fastify from "fastify"
+
 
 const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/hello', async () => {
+    const users = await prisma.user.findMany()
+
+    return users
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('🚀 O servidor está online! 🚀')
+})
